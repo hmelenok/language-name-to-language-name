73121de871f92c4fc3b8c78dd395b1e6a4018122
Khmel Mykyta <hmelenok@gmail.com>
2020-09-10 15:59:32 +0300

    feat: kill lodash

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-import { map, reduce } from 'lodash';
 import dir from './maps/dir';
 import native from './maps/native';
 import languages from './maps/languages';
 import { LanguageNameMap, LanguageCode } from './types';
 
-export const langToLang = (language: LanguageCode = 'en'): LanguageNameMap => reduce(
-  map(languages[language], (value, lang) => ({
-    [lang]: {
-      name: value,
-      native: native[lang],
-      dir: dir[lang],
-    },
-  })),
-  (value, memo) => ({ ...memo, ...value }),
-  {} as any,
-);
+export const langToLang = (language: LanguageCode = 'en'): LanguageNameMap => {
+  const targetLanguage = languages[language];
+  const keys = Object.keys(targetLanguage);
+
+  return keys
+    .map((currentLanguage) => ({
+      value: targetLanguage[currentLanguage],
+      lang: currentLanguage,
+    }))
+    .map(({ value, lang }) => ({
+      [lang]: {
+        name: value,
+        native: native[lang],
+        dir: dir[lang],
+      },
+    }))
+    .reduce((value, memo) => ({ ...memo, ...value }), {} as any);
+};
 
 export default langToLang;
